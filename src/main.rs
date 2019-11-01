b33d653c6820273599feb3123485c63ad4622477
Yuta Nagaoka <shirasu.okatin@gmail.com>
2019-11-02 00:03:14 +0900

    feat: Fetch contest page, parse url, and extract sample cases.
    And enable to skip input example.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 extern crate reqwest;
 extern crate scraper;
 
-use scraper::{Selector, Html};
 use reqwest::Url;
+use scraper::{Html, Selector};
 
 fn main() {
     let url = "https://atcoder.jp/contests/abc144/tasks/abc144_e";
-    let html = fetch_html(url);
-    let doc = Html::parse_document(&(html.unwrap()));
-    let selector = Selector::parse("pre").unwrap();
-    for node in doc.select(&selector) {
-        println!("---One iteration---");
-        let text = node.text().collect::<Vec<_>>();
-        println!("{:?}", text);
-        println!("{:?}", node.value().id());
+    let sample_cases = parse_sample_case(url);
+    for case in sample_cases {
+        println!("{}", case);
     }
+}
+
+fn parse_sample_case(url: &str) -> Vec<String> {
+    let result = fetch_html(url);
+    let html = Html::parse_document(&(result.unwrap()));
+
+    let selector_lang_ja = Selector::parse("span.lang-ja").unwrap();
+    let html_lang_ja = html.select(&selector_lang_ja).nth(0).unwrap().html();
+    let selector_sample_case = Selector::parse("pre").unwrap();
+    let html_sample_case = Html::parse_fragment(&html_lang_ja);
+    let elem_sample_case = html_sample_case.select(&selector_sample_case);
+
+    let mut sample_cases: Vec<String> = Vec::new();
+    for node in elem_sample_case {
+        // Skip element of input example.
+        if node.children().count() > 1 {
+            continue;
+        }
+        let text = node.text().collect::<Vec<&str>>().join("");
+        sample_cases.push(text);
+    }
+    sample_cases
 }
 
 fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
