7ae1f104ecb3014b25abf95aa0975c465d4bbef1
Yuta Nagaoka <shirasu.okatin@gmail.com>
2019-11-02 11:48:29 +0900

    feat: Add struct`SampleCases` to hold input/output examples.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-mod fetch_sample_cases;
+mod sample_cases;
 
 fn main() {
     let url = "https://atcoder.jp/contests/abc144/tasks/abc144_e";
-    let sample_cases = fetch_sample_cases::parse_sample_cases(url);
-    for case in sample_cases {
-        println!("{}", case);
+    let sc = sample_cases::SampleCases::new(url);
+    for input in sc.input {
+        println!("INPUT:\n{}", input);
+    }
+    for output in sc.output {
+        println!("OUTPUT:\n{}", output);
     }
 }
