9ef1a4610488b96a1f438c2d5b80443b7bdc7c12
Yuta Nagaoka <shirasu.okatin@gmail.com>
2019-11-02 10:27:00 +0900

    update: Split into multiple files.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,9 @@
-extern crate reqwest;
-extern crate scraper;
-
-use reqwest::Url;
-use scraper::{Html, Selector};
+mod fetch_sample_cases;
 
 fn main() {
     let url = "https://atcoder.jp/contests/abc144/tasks/abc144_e";
-    let sample_cases = parse_sample_case(url);
+    let sample_cases = fetch_sample_cases::parse_sample_cases(url);
     for case in sample_cases {
         println!("{}", case);
     }
-}
-
-fn parse_sample_case(url: &str) -> Vec<String> {
-    let result = fetch_html(url);
-    let html = Html::parse_document(&(result.unwrap()));
-
-    let selector_lang_ja = Selector::parse("span.lang-ja").unwrap();
-    let html_lang_ja = html.select(&selector_lang_ja).nth(0).unwrap().html();
-    let selector_sample_case = Selector::parse("pre").unwrap();
-    let html_sample_case = Html::parse_fragment(&html_lang_ja);
-    let elem_sample_case = html_sample_case.select(&selector_sample_case);
-
-    let mut sample_cases: Vec<String> = Vec::new();
-    for node in elem_sample_case {
-        // Skip element of input example.
-        if node.children().count() > 1 {
-            continue;
-        }
-        let text = node.text().collect::<Vec<&str>>().join("");
-        sample_cases.push(text);
-    }
-    sample_cases
-}
-
-fn fetch_html(url: &str) -> Result<String, reqwest::Error> {
-    let parsed_url = Url::parse(url).unwrap();
-    let mut res = reqwest::get(parsed_url).unwrap();
-    res.text()
 }
