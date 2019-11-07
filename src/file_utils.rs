754fe8f189044beb2da688e15d9ad5c805617ba5
Yuta Nagaoka <shirasu.okatin@gmail.com>
2019-11-07 22:34:05 +0900

    feat: Create input/output example files under `input` or `output` directories.

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -1,21 +1,38 @@
+use std::env;
 use std::io;
 use std::io::Write;
-use std::fs::File;
+use std::fs::{ self, File, DirBuilder };
 use crate::sample_cases::SampleCases;
+use std::path::Path;
 
 
 // Create file of sample cases for each element of `SampleCases` struct.
 pub fn create_test_files(sc: SampleCases) -> Result<(), io::Error> {
+    create_directory("input")?;
+    create_directory("output")?;
+
     for (i, input_example) in sc.input.iter().enumerate() {
-        let file_name = format!("input{}.txt", i + 1);
+        let file_name = format!(r"input\input{}.txt", i + 1);
         create_test_file(input_example, file_name).expect("Failed to create file.");
     }
 
     for (i, output_example) in sc.output.iter().enumerate() {
-        let file_name = format!("output{}.txt", i + 1);
+        let file_name = format!(r"output\output{}.txt", i + 1);
         create_test_file(output_example, file_name).expect("Failed to create file.");
     }
     Ok(())
+}
+
+// Create directory named `dir_name` under the current directory.
+fn create_directory(dir_name: &str) -> io::Result<()> {
+    let mut dir = env::current_dir()?;
+    dir.push(dir_name);
+    if dir.is_dir() {
+        Ok(())
+    }
+    else {
+        DirBuilder::new().create(dir)
+    }
 }
 
 // Write `file_content` on a file.
