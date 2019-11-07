0256cc9e01f4f64b42a2f98c173da301450cac8c
Yuta Nagaoka <shirasu.okatin@gmail.com>
2019-11-07 22:50:02 +0900

    update: Ready for publish.

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -12,18 +12,18 @@ pub fn create_test_files(sc: SampleCases) -> Result<(), io::Error> {
     create_directory("output")?;
 
     for (i, input_example) in sc.input.iter().enumerate() {
-        let file_name = format!(r"input\input{}.txt", i + 1);
+        let file_name = format!(r"input/input{}.txt", i + 1);
         create_test_file(input_example, file_name).expect("Failed to create file.");
     }
 
     for (i, output_example) in sc.output.iter().enumerate() {
-        let file_name = format!(r"output\output{}.txt", i + 1);
+        let file_name = format!(r"output/output{}.txt", i + 1);
         create_test_file(output_example, file_name).expect("Failed to create file.");
     }
     Ok(())
 }
 
-// Create directory named `dir_name` under the current directory.
+// Create directory named `dir_name` under the current directory if it doesn't exist.
 fn create_directory(dir_name: &str) -> io::Result<()> {
     let mut dir = env::current_dir()?;
     dir.push(dir_name);
